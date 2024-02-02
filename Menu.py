5c45581332acbb0ae40ba5c4c7d07b6a0042bc94
mikannohako <mikannokawa1124@gmail.com>
2024-02-02 22:18:37 +0900

    初期起動ボタンの設定更新

--- Menu.py
+++ Menu.py
@@ -3,17 +3,29 @@ import PySimpleGUI as sg
 import sys
 import os
 import subprocess
+from datetime import datetime
 
 # GUI画面のレイアウト
 layout = [
     [sg.Text("起動する機能を選んでください。", font=("Helvetica", 15), justification='center')],  # カンマを追加
     [sg.Button('通常出席', bind_return_key=True, font=("Helvetica", 15)),
         sg.Button('Excel整理', bind_return_key=True, font=("Helvetica", 15)),
-        sg.Button('初期起動', bind_return_key=True, font=("Helvetica", 15)),
+        sg.Button('初期起動', bind_return_key=True, font=("Helvetica", 15), key='-SYOKI-'),
         sg.Button('終了', bind_return_key=True, font=("Helvetica", 15))]
 ]
 
 window = sg.Window('出席処理', layout, finalize=True)
+
+current_date = datetime.now()
+current_date_y = current_date.strftime("%Y")
+
+window['-SYOKI-'].update(disabled=False)
+
+# 一時ファイル名
+ar_filename = f"{current_date_y}Attendance records.xlsx"
+
+if os.path.exists(ar_filename):
+    window['-SYOKI-'].update(disabled=True)
 
 while True:  #? 無限ループ
     event, values = window.read()
