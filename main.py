85f2c2daa66b68e3b43a5dd1755be58225b95854
mikannohako <mikannokawa1124@gmail.com>
2024-02-08 16:12:37 +0900

    ファイルの一本化

--- main.py
+++ main.py
@@ -12,19 +12,8 @@ from openpyxl.styles import PatternFill
 from openpyxl import load_workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
 import json
-import PySimpleGUI as sg
-import sys
-import os
-from datetime import datetime
-from tkinter import messagebox
-import sqlite3
-import openpyxl
-from openpyxl.styles import PatternFill
-from openpyxl import load_workbook
 import cv2
 import time
-import json
-import subprocess
 
 #? エラー時の処理の作成
 
@@ -121,6 +110,15 @@ def SApy(): #? SpecialAttendance.py
 
 def GApy(): #? GeneralAttendance.py & ExcelClean.py
     #? config設定
+    
+    # 時間変数の設定
+    current_date = datetime.now()
+    current_date_y = current_date.strftime("%Y")
+    current_date_m = current_date.strftime("%m")
+    current_date_d = current_date.strftime("%d")
+    current_date_h = current_date.strftime("%H")
+    current_date_M = current_date.strftime("%M")
+    current_date_s = current_date.strftime("%S")
     
     # 設定ファイルのパス
     config_file_path = 'config.json'
@@ -328,14 +326,9 @@ def GApy(): #? GeneralAttendance.py & ExcelClean.py
                 
                 workbook.save(ar_filename)
                 
+                
                 #? excelの色付け
                 print("Color Change >>> ", end="")
-                
-                #? エラー時の処理の作成
-                def exit_with_error(message):
-                    print(f"Error: {message}")
-                    messagebox.showerror("Error:", message)
-                    sys.exit(1)  # アプリケーションをエラーコード 1 で終了します
                 
                 # 時間変数の設定
                 current_date = datetime.now()
@@ -370,21 +363,14 @@ def GApy(): #? GeneralAttendance.py & ExcelClean.py
                 
                 print("done")
                 
-                messagebox.showinfo('完了', '正常に終了しました。')
-                
-                subprocess.run(["python", "Menu.py"])
-                sys.exit(0)
-                
                 messagebox.showinfo('完了', '記録終了は正常に終了しました。')
-                conn.close()
+                window.close()
                 break
             else:
                 # ウィンドウを閉じてから新しいウィンドウを作成
                 window.close()
                 window = mainwindowshow()
                 continue
-
-
 
 
 # GUI画面のレイアウト
@@ -394,7 +380,7 @@ layout = [
         sg.Button('終了', bind_return_key=True, font=("Helvetica", 15))]
 ]
 
-menu = sg.Window('出席処理', layout, finalize=True)
+menu = sg.Window('MENU', layout, finalize=True, keep_on_top=True)
 
 # 記録ファイル名
 ar_filename = f"{current_date_y}Attendance records.xlsx"
@@ -403,6 +389,7 @@ ar_filename = f"{current_date_y}Attendance records.xlsx"
 if not os.path.exists(ar_filename):
     # ファイルが存在しない場合の処理
     SApy()
+
 
 while True:  #? 無限ループ
     event, values = menu.read()
@@ -413,5 +400,9 @@ while True:  #? 無限ループ
     
     if event == '通常出席':
         menu.close()
+        
         GApy()
-        sys.exit(0)
+        messagebox.showinfo('インフォメーション', 'メニューが再度起動します。\nお待ちください。')
+        
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
